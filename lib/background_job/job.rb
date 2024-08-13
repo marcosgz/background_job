9bd6d8758955bb08a4520dff60ebb277b89192da
Marcos G. Zimmermann <mgzmaster@gmail.com>
2024-08-13 13:12:17 -0300

    feat: create a job dsl to initalize a Job to be pushed to the service

---
+++ lib/background_job/job.rb
@@ -0,0 +1,129 @@
+# frozen_string_literal: true
+
+require_relative './unique_job'
+
+module BackgroundJob
+  # DSL used to create a job. It's generic so it can be used with any adapter.
+  class Job
+    attr_reader :options, :payload, :job_class, :unique_job
+
+    attr_reader :arguments
+
+    # def self.coerce(payload:, **opts)
+    #   SERVICES.fetch(service).coerce_to_worker(payload, **opts)
+    # end
+
+    def initialize(job_class, **options)
+      @job_class = job_class
+      @options = options
+      @payload = {}
+      unique(@options.delete(:uniq)) if @options.key?(:uniq)
+    end
+
+    # @return [Symbol] The service key
+    #
+    # @abstract
+    def service
+      raise NotImplementedError
+    end
+
+    # Push the job to the service backend
+    #
+    # @abstract
+    def push
+      raise NotImplementedError
+    end
+
+    %i[created_at enqueued_at].each do |method_name|
+      define_method method_name do |value|
+        @payload[method_name.to_s] = \
+          case value
+          when Numeric then value.to_f
+          when String then Time.parse(value).to_f
+          when Time, DateTime then value.to_f
+          else
+            raise ArgumentError, format('The %<v>p is not a valid value for %<m>s.', v: value, m: method_name)
+          end
+
+        self
+      end
+    end
+
+    # Adds arguments to the job
+    # @return self
+    def with_args(*args)
+      @payload['args'] = args
+
+      self
+    end
+
+    # Schedule the time when a job will be executed. Jobs which are scheduled in the past are enqueued for immediate execution.
+    # @param timestamp [Numeric] timestamp, numeric or something that acts numeric.
+    # @return self
+    def in(timestamp)
+      now = Time.now.to_f
+      timestamp = Time.parse(timestamp) if timestamp.is_a?(String)
+      int = timestamp.respond_to?(:strftime) ? timestamp.to_f : now + timestamp.to_f
+      return self if int <= now
+
+      @payload['at'] = int
+      @payload['created_at'] = now
+
+      self
+    end
+    alias_method :at, :in
+
+    # Wrap uniq options
+    #
+    # @param value [Hash] Unique configurations with `across`, `timeout` and `unlock_policy`
+    # @return self
+    def unique(value)
+      value = {} if value == true
+      @unique_job = \
+        case value
+        when Hash then UniqueJob.coerce(value)
+        when UniqueJob then value
+        else
+          nil
+        end
+
+      self
+    end
+
+    def with_job_jid(jid = nil)
+      @payload['jid'] ||= jid || BackgroundJob.jid
+
+      self
+    end
+
+    # # @param :to [Symbol] Adapter key
+    # # @return Response of service
+    # # @see BackgroundJob::Adapters::** for more details
+    # def push(to: nil)
+    #   to ||= options[:service]
+    #   unless SERVICES.include?(to)
+    #     raise Error, format('Service %<to>p is not implemented. Please use one of %<list>p', to: to, list: SERVICES.keys)
+    #   end
+
+    #   @payload['created_at'] ||= Time.now.to_f
+    #   worker_to_push = with_job_jid
+    #   BackgroundJob.config.middleware.invoke(worker_to_push, to) do
+    #     SERVICES[to].push(worker_to_push)
+    #   end
+    # end
+
+    def eql?(other)
+      return false unless other.is_a?(self.class)
+
+      job_class == other.job_class && \
+        payload == other.payload &&
+        options == other.options &&
+        unique_job == other.unique_job
+    end
+    alias == eql?
+
+    def unique_job?
+      unique_job.is_a?(UniqueJob)
+    end
+  end
+end
---
+++ spec/background_job/job_spec.rb
@@ -0,0 +1,329 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe BackgroundJob::Job do
+  let(:worker) { described_class.new('DummyWorker') }
+
+  describe '#options' do
+    specify do
+      expected_options = described_class.new('DummyWorker').options
+      expect(expected_options).to eq({})
+    end
+
+    specify do
+      expected_options = described_class.new('DummyWorker', retry: true).options
+      expect(expected_options).to eq(retry: true)
+    end
+  end
+
+  describe '#created_at', freeze_at: [2020, 7, 1, 22, 24, 40] do
+    let(:now) { Time.now }
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.created_at(now)).to eq(worker)
+      expect(worker.payload).to eq('created_at' => now.to_f)
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.created_at(Time.now)).to eq(worker)
+      expect(worker.payload).to eq('created_at' => now.to_f)
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.created_at(Time.now.to_datetime.rfc3339(9))).to eq(worker)
+      expect(worker.payload).to eq('created_at' => now.to_f)
+    end
+  end
+
+  describe '#enqueued_at', freeze_at: [2020, 7, 1, 22, 24, 40] do
+    let(:now) { Time.now }
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.enqueued_at(now)).to eq(worker)
+      expect(worker.payload).to eq('enqueued_at' => now.to_f)
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.enqueued_at(Time.now)).to eq(worker)
+      expect(worker.payload).to eq('enqueued_at' => now.to_f)
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.enqueued_at(Time.now.to_datetime.rfc3339(9))).to eq(worker)
+      expect(worker.payload).to eq('enqueued_at' => now.to_f)
+    end
+  end
+
+  describe '#with_args' do
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.with_args()).to eq(worker)
+      expect(worker.payload).to eq('args' => [])
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.with_args(1)).to eq(worker)
+      expect(worker.payload).to eq('args' => [1])
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.with_args(1, foo: :bar)).to eq(worker)
+      expect(worker.payload).to eq('args' => [1, { foo: :bar }])
+    end
+  end
+
+  describe '#at', freeze_at: [2020, 7, 1, 22, 24, 40] do
+    let(:now) { Time.now }
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.at(10)).to eq(worker)
+      expect(worker.payload).to eq('at' => now.to_f + 10, 'created_at' => now.to_f)
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.at(now + HOUR_IN_SECONDS)).to eq(worker)
+      expect(worker.payload).to eq('at' => now.to_f + 3_600, 'created_at' => now.to_f)
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.at((now + HOUR_IN_SECONDS).to_datetime.rfc3339(9))).to eq(worker)
+      expect(worker.payload).to eq('at' => now.to_f + 3_600, 'created_at' => now.to_f)
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.at(0)).to eq(worker)
+      expect(worker.payload).to eq({})
+      expect(worker.at(now - 1)).to eq(worker)
+      expect(worker.payload).to eq({})
+    end
+  end
+
+  describe '#in', freeze_at: [2020, 7, 1, 22, 24, 40] do
+    let(:now) { Time.now }
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.in(10)).to eq(worker)
+      expect(worker.payload).to eq('at' => now.to_f + 10, 'created_at' => now.to_f)
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.in(now + HOUR_IN_SECONDS)).to eq(worker)
+      expect(worker.payload).to eq('at' => now.to_f + 3_600, 'created_at' => now.to_f)
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.in((now + HOUR_IN_SECONDS).to_datetime.rfc3339(9))).to eq(worker)
+      expect(worker.payload).to eq('at' => now.to_f + 3_600, 'created_at' => now.to_f)
+    end
+
+    specify do
+      expect(worker.payload).to eq({})
+      expect(worker.in(0)).to eq(worker)
+      expect(worker.payload).to eq({})
+      expect(worker.in(now - 1)).to eq(worker)
+      expect(worker.payload).to eq({})
+    end
+  end
+
+  describe '#unique' do
+    specify do
+      worker = described_class.new('DummyWorker')
+      expect(worker.unique_job).to eq(nil)
+      expect(worker.unique(true)).to eq(worker)
+      expect(worker).to be_unique_job
+      expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+      expect(worker.unique(false)).to eq(worker)
+      expect(worker.unique_job).to eq(nil)
+      expect(worker).not_to be_unique_job
+    end
+
+    specify do
+      worker = described_class.new('DummyWorker')
+      expect(worker.unique_job).to eq(nil)
+      expect(worker.unique(across: :systemwide)).to eq(worker)
+      expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+      expect(worker).to be_unique_job
+    end
+
+    specify do
+      worker = described_class.new('DummyWorker')
+      expect(worker.unique_job).to eq(nil)
+      unique_job = BackgroundJob::UniqueJob.new
+      expect(worker.unique(unique_job)).to eq(worker)
+      expect(worker.unique_job).to eq(unique_job)
+      expect(worker).to be_unique_job
+    end
+  end
+
+  describe '#unique_job' do
+    let(:defaults) do
+      {
+        across: :queue,
+        timeout: WEEK_IN_SECONDS,
+        unlock_policy: :success,
+      }
+    end
+
+    specify do
+      worker = described_class.new('DummyWorker')
+      expect(worker.payload).to eq({})
+      expect(worker.options).to eq({})
+      expect(worker.unique_job).to eq(nil)
+    end
+
+    specify do
+      worker = described_class.new('DummyWorker', uniq: {})
+      expect(worker.payload).to eq({})
+      expect(worker.options).to eq({})
+      expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+    end
+
+    specify do
+      worker = described_class.new('DummyWorker', uniq: true)
+      expect(worker.payload).to eq({})
+      expect(worker.options).to eq({})
+      expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+    end
+
+    specify do
+      worker = described_class.new('DummyWorker', uniq: false)
+      expect(worker.payload).to eq({})
+      expect(worker.options).to eq({})
+      expect(worker.unique_job).to eq(nil)
+    end
+
+    context 'with custom :across option' do
+      specify do
+        expect { described_class.new('DummyWorker', uniq: { across: :invalid }) }.to raise_error(
+          BackgroundJob::Error, 'Invalid `across: :invalid` option. Only [:queue, :systemwide] are allowed.'
+        )
+      end
+
+      specify do
+        worker = described_class.new('DummyWorker', uniq: { across: :queue })
+        expect(worker.options).to eq({})
+        expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+        expect(worker.unique_job.across).to eq(:queue)
+      end
+
+      specify do
+        worker = described_class.new('DummyWorker', uniq: { across: :systemwide })
+        expect(worker.payload).to eq({})
+        expect(worker.options).to eq({})
+        expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+        expect(worker.unique_job.across).to eq(:systemwide)
+      end
+
+      specify do
+        worker = described_class.new('DummyWorker', uniq: { across: 'systemwide' })
+        expect(worker.payload).to eq({})
+        expect(worker.options).to eq({})
+        expect(worker.unique_job.across).to eq(:systemwide)
+      end
+    end
+
+    context 'with custom :unlock_policy option' do
+      specify do
+        expect { described_class.new('DummyWorker', uniq: { unlock_policy: :invalid }) }.to raise_error(
+          BackgroundJob::Error, 'Invalid `unlock_policy: :invalid` option. Only [:success, :start] are allowed.'
+        )
+      end
+
+      specify do
+        worker = described_class.new('DummyWorker', uniq: { unlock_policy: :success })
+        expect(worker.payload).to eq({})
+        expect(worker.options).to eq({})
+        expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+        expect(worker.unique_job.unlock_policy).to eq(:success)
+      end
+
+      specify do
+        worker = described_class.new('DummyWorker', uniq: { unlock_policy: :start })
+        expect(worker.payload).to eq({})
+        expect(worker.options).to eq({})
+        expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+        expect(worker.unique_job.unlock_policy).to eq(:start)
+      end
+
+      specify do
+        worker = described_class.new('DummyWorker', uniq: { unlock_policy: 'start' })
+        expect(worker.payload).to eq({})
+        expect(worker.options).to eq({})
+        expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+        expect(worker.unique_job.unlock_policy).to eq(:start)
+      end
+    end
+
+    context 'with custom :timeout option' do
+      specify do
+        worker = described_class.new('DummyWorker', uniq: { timeout: -1 })
+        expect(worker.payload).to eq({})
+        expect(worker.options).to eq({})
+        expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+        expect(worker.unique_job.timeout).to eq(defaults[:timeout])
+      end
+
+      specify do
+        worker = described_class.new('DummyWorker', uniq: { timeout: 10 })
+        expect(worker.payload).to eq({})
+        expect(worker.options).to eq({})
+        expect(worker.unique_job).to be_an_instance_of(BackgroundJob::UniqueJob)
+        expect(worker.unique_job.timeout).to eq(10)
+      end
+    end
+  end
+
+  describe '#eql?' do
+    context 'worker class name' do
+      specify do
+        expect(described_class.new('Foo')).to eql(described_class.new('Foo'))
+        expect(described_class.new('Foo')).to eq(described_class.new('Foo'))
+      end
+
+      specify do
+        expect(described_class.new('Bar')).not_to eql(described_class.new('Foo'))
+        expect(described_class.new('Bar')).not_to eq(described_class.new('Foo'))
+      end
+    end
+
+    context 'job content' do
+      specify do
+        expect(described_class.new('Foo').with_args([1])).to eql(described_class.new('Foo').with_args([1]))
+        expect(described_class.new('Foo').with_args([1])).to eq(described_class.new('Foo').with_args([1]))
+      end
+
+      specify do
+        expect(described_class.new('Foo').with_args([1])).not_to eql(described_class.new('Foo').with_args([2]))
+        expect(described_class.new('Foo').with_args([1])).not_to eq(described_class.new('Foo').with_args([2]))
+      end
+    end
+
+    context 'worker options' do
+      specify do
+        expect(described_class.new('Foo', queue: 'foo')).to eql(described_class.new('Foo', queue: 'foo'))
+        expect(described_class.new('Foo', queue: 'foo')).to eq(described_class.new('Foo', queue: 'foo'))
+      end
+
+      specify do
+        expect(described_class.new('Foo', queue: 'foo')).not_to eql(described_class.new('Foo', queue: 'bar'))
+        expect(described_class.new('Foo', queue: 'foo')).not_to eq(described_class.new('Foo', queue: 'bar'))
+      end
+    end
+  end
+end
